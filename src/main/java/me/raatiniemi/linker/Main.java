a8c5484fcf39888b91e14d4ad6a1937230cb6f96
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-10-02 22:24:05 +0200

    Check that configuration file have been supplied

--- src/main/java/me/raatiniemi/linker/Main.java
+++ src/main/java/me/raatiniemi/linker/Main.java
@@ -2,5 +2,11 @@ package me.raatiniemi.linker;
 
 public class Main {
     public static void main(String... args) {
+        // Check that we have been supplied with a configuration file.
+        if (0 == args.length || args[0].isEmpty()) {
+            throw new RuntimeException(
+                    "No configuration file have been supplied"
+            );
+        }
     }
 }

4cc160dd929aefbb49f5bbab026604090b5dce6b
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-10-02 22:40:42 +0200

    Read 'source.directory' from configuration file

--- src/main/java/me/raatiniemi/linker/Main.java
+++ src/main/java/me/raatiniemi/linker/Main.java
@@ -1,11 +1,13 @@
 package me.raatiniemi.linker;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 public class Main {
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
         // Check that we have been supplied with a configuration file.
         if (0 == args.length || args[0].isEmpty()) {
             throw new RuntimeException(
@@ -18,6 +20,18 @@ public class Main {
         if (!Files.exists(configuration)) {
             throw new RuntimeException(
                     "Configuration file do not exist"
+            );
+        }
+
+        // Load the configuration file into properties.
+        Properties properties = new Properties();
+        properties.load(Files.newInputStream(configuration));
+
+        // Attempt to read the source directory.
+        String sourceDirectory = properties.getProperty("source.directory");
+        if (sourceDirectory.isEmpty()) {
+            throw new RuntimeException(
+                    "No source directory have been supplied"
             );
         }
     }

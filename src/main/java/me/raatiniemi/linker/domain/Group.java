f999e4990f386c85d7c05f0e198e1174a255abb8
Tobias Raatiniemi <raatiniemi@gmail.com>
2015-10-04 14:55:50 +0200

    Add container for items within group

--- src/main/java/me/raatiniemi/linker/domain/Group.java
+++ src/main/java/me/raatiniemi/linker/domain/Group.java
@@ -1,9 +1,32 @@
 package me.raatiniemi.linker.domain;
 
 import java.nio.file.Path;
+import java.util.List;
 
 public class Group extends AbstractDirectory {
-    public Group(Path path) {
+    /**
+     * Items contained within the group.
+     */
+    private List<Path> items;
+
+    /**
+     * Constructor.
+     *
+     * @param path Path for the group.
+     * @param items Items contained within the group.
+     */
+    public Group(Path path, List<Path> items) {
         super(path);
+
+        this.items = items;
+    }
+
+    /**
+     * Getter method for items contained within the group.
+     *
+     * @return Items contained within the group.
+     */
+    public List<Path> getItems() {
+        return this.items;
     }
 }

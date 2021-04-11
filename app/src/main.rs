a2ca9921c37ce12edf1d3bd5ea3d25150e6736d3
Tobias Raatiniemi <raatiniemi@gmail.com>
2021-04-11 08:49:38 +0200

    clean up initialization for collect and filter nodes

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::path::PathBuf;
 
-use crate::configuration::read_configuration;
+use crate::configuration::{read_configuration, Configuration};
 use crate::collect_nodes::collect_nodes;
 use crate::filter_source_nodes::filter_source_nodes;
 use crate::filter_target_nodes::filter_target_nodes;
@@ -19,21 +19,31 @@ fn main() {
             let configuration = read_configuration(&path);
             println!("{:?}", configuration);
 
-            let source = configuration.source.unwrap();
-            let source_path = PathBuf::from(source.as_str());
-            let source_nodes = collect_nodes(&source_path);
-
-            let target_nodes = configuration.targets
-                .iter()
-                .map(|target| PathBuf::from(target.as_str()))
-                .flat_map(|target_path| collect_nodes(&target_path))
-                .to_owned()
-                .collect::<Vec<Node>>();
-
-            filter(&filter_source_nodes(&source_nodes, &configuration.excludes), &filter_target_nodes(&target_nodes))
+            let source_nodes = collect_and_filter_source_nodes(&configuration);
+            let target_nodes = collect_and_filter_target_nodes(&configuration);
+            filter(&source_nodes, &target_nodes)
                 .iter()
                 .for_each(|n| println!("{:?}", n));
         }
         _ => panic!("No path for configuration is available")
     };
+}
+
+fn collect_and_filter_source_nodes(configuration: &Configuration) -> Vec<Node> {
+    let source_nodes = configuration.source.clone()
+        .map(|v| PathBuf::from(v.as_str()))
+        .map(|v| collect_nodes(&v))
+        .expect(&format!("Unable to read path for sources from configuration"));
+
+    filter_source_nodes(&source_nodes, &configuration.excludes)
+}
+
+fn collect_and_filter_target_nodes(configuration: &Configuration) -> Vec<Node> {
+    let target_nodes = &configuration.targets.clone()
+        .iter()
+        .map(|v| PathBuf::from(v.as_str()))
+        .flat_map(|v| collect_nodes(&v))
+        .collect::<Vec<Node>>();
+
+    filter_target_nodes(&target_nodes)
 }

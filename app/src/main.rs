0c7ac6f77dcb6f7caa91f1b5dba1528acbfd7c6b
Tobias Raatiniemi <raatiniemi@gmail.com>
2021-05-22 21:15:58 +0200

    collect target nodes in parallel

--- app/src/main.rs
+++ app/src/main.rs
@@ -77,7 +77,7 @@ fn collect_and_filter_source_nodes(tracer: &trace::Tracer, configuration: &Confi
 fn collect_and_filter_target_nodes(tracer: &trace::Tracer, configuration: &Configuration) -> Vec<Node> {
     tracer.in_span("collect_and_filter_target_nodes", |_| {
         let target_nodes = &configuration.targets.clone()
-            .iter()
+            .par_iter()
             .map(|v| PathBuf::from(v.as_str()))
             .flat_map(|v| collect_nodes(&v))
             .collect::<Vec<Node>>();
